86b7ec371dd4a959523c769b39230a35e19d7dff
Tomas <tvaitilas@gmail.com>
2023-01-15 19:41:00 +0200

    add: conditions and loops

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -471,8 +471,40 @@ function getMatrixProduct(m1, m2) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
+
+function evaluateTicTacToePosition(position) {
+  for (let i = 0; i < 3; i += 1) {
+    if (
+      position[i][0] &&
+      position[i][0] === position[i][1] &&
+      position[i][2] === position[i][0]
+    ) {
+      return position[i][0];
+    }
+    if (
+      position[0][i] &&
+      position[0][i] === position[1][i] &&
+      position[2][i] === position[0][i]
+    ) {
+      return position[0][i];
+    }
+  }
+  if (
+    position[0][0] &&
+    position[0][0] === position[1][1] &&
+    position[2][2] === position[0][0]
+  ) {
+    return position[1][1];
+  }
+  if (
+    position[2][0] &&
+    position[2][0] === position[1][1] &&
+    position[1][1] === position[0][2]
+  ) {
+    return position[1][1];
+  }
+  return undefined;
+  // throw new Error('Not implemented');
 }
 
 module.exports = {
